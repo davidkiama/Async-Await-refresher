8603938127b93cb4b9ccce4c5b5030a18989b0bc
davidkiama <davidkiama98@gmail.com>
2022-07-17 21:25:02 +0300

    Initial refactor to Promises

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const superagent = require("superagent");
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Dog breed:${data}`);
 
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    if (err) return console.log(err);
-    console.log(res.body.message);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
 
-    fs.writeFile("dog-image.txt", res.body.message, (err) => {
-      if (err) return console.log(err);
+      fs.writeFile("dog-image.txt", res.body.message, (err) => {
+        if (err) return console.log(err);
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
-  });
 });
